aabcfcad9393d3f429c6fc6d339add5279469a6f
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-08-08 00:15:31 -0400

    Time: 114 ms (19.79%), Space: 40 MB (68.73%) - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    //Naive solution: sliding window from start to end, calculating each product and storing it. Once pointer reaches end, move start pointer up. Repeat until start = end. Then take the max of stored products. 
+    
+    //O(n) Solution
+    //Loop forwards building product, checking max every time
+    //Loop backwards building product, checking max every time
+    //If a zero is met, reset product to 1
+    //Problem dependent on even or odd number of negative integers
+    //[-3, -1, -10] -> loop forwards, max -3*-1 = 3, loop backwards, max -10*-1 = 10
+   
+        let prod = 1;
+        let result = Math.max(...nums);
+        
+        for (let i = 0; i < nums.length; i++) {
+            prod = prod * nums[i];
+            result = Math.max(prod, result);
+            if (prod === 0) {
+                prod = 1;
+            }
+        }
+    
+        prod = 1;
+        
+        for (let j = nums.length - 1; j >= 0; j--) {
+            prod = prod * nums[j];
+            result = Math.max(prod, result);
+            if (prod === 0) {
+                prod = 1;
+            }      
+        }
+        
+        return result;
+    
+};
