b5b88836109691171453d9e372d600595a8028a4
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-08-12 13:04:10 -0400

    Time: 76 ms (90.89%), Space: 41 MB (69.95%) - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -3,28 +3,29 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-   if (!grid || grid.length === '0') return 0;
+    if (!grid || grid.length === 0) return 0;
     
-    let num_islands = 0;
+    let num_islands = 0; //3
     
-    const sinkIslands = (grid, i, j) => {
-        if (i < 0 || i === grid.length || j < 0 || j === grid[i].length || grid[i][j] === '0') {
+    const dfs = (grid, i, j) => {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === '0') {
             return;
         }
         
         grid[i][j] = '0';
         
-        sinkIslands(grid, i + 1, j);
-        sinkIslands(grid, i - 1, j);
-        sinkIslands(grid, i, j - 1);
-        sinkIslands(grid, i, j + 1);
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
     }
+    
     
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] === '1') {
                 num_islands++;
-                sinkIslands(grid, i, j);
+                dfs(grid, i, j);
             }
         }
     }
