f4d4bef93edc5e5184dfdcdbb9777a22dbf039fd
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-08-18 23:02:34 -0400

    Time: 256 ms (31.06%), Space: 69 MB (24.01%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.js
+++ non-overlapping-intervals/non-overlapping-intervals.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    if (intervals.length === 1) return 0;
+    if (!intervals || intervals.length === 1) return 0;
     
     intervals.sort((a,b) => a[1] - b[1]);
-    let end = intervals[0][1];
-    let count = 1; //2
     
+    let end = intervals[0][1];
+    let count = 0;
     
     for (let i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] >= end) {
-                end = intervals[i][1];
-                count++;
-            }
+        if (intervals[i][0] < end) {
+            count++;
+        } else {
+            end = intervals[i][1];
+        }
     }
     
-    return intervals.length - count;
+    return count;
 };
