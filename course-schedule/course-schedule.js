6d9534562c41b3745c3c5fe225c05c2519dd0411
Vlad Trebukhov <vlad.trebukhov123@gmail.com>
2021-08-29 00:01:48 -0400

    Time: 1917 ms (5.29%), Space: 46.3 MB (21.15%) - LeetHub

--- course-schedule/course-schedule.js
+++ course-schedule/course-schedule.js
@@ -3,23 +3,15 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function(numCourses, prerequisites) {
-    //1, [] -> true
-    //2, [[1,0]] -> true
-    //3, [[1,0],[1,2],[2,1] -> false
-    //6, [[1,0], [2,1], [3,2], [4,3]] -> true, 
-    //3, [[0,1],[2,0],[1,2]] -> false, no way to take course 1
-    //4, [[0,1],[1,2],[3,2]] -> true
-    
+var canFinish = function(numCourses, prerequisites) { 
     if (!prerequisites.length) return true;
     
-    //make map of pre-reqs
-    
-    let prereqs_map = {}; //{1: [0,2], 2: [1]}
+    let prereqs_map = {};
     
     for (let i  = 0; i < prerequisites.length; i++) {
         let a = prerequisites[i][0];
         let b = prerequisites[i][1];
+        
         if (!(a in prereqs_map)) {
             prereqs_map[a] = [b]
         } else {
@@ -27,8 +19,8 @@ var canFinish = function(numCourses, prerequisites) {
         }
     }
 
-    let courses = Object.keys(prereqs_map); //[0, 1, 3]
-    let stack = []; //[]
+    let courses = Object.keys(prereqs_map); 
+    let stack = [];
 
     for (let j = 0; j < courses.length; j++) {
         let checked = [];
@@ -37,7 +29,7 @@ var canFinish = function(numCourses, prerequisites) {
         stack.push(prereqs_map[course])
         
         while (stack.length) {
-            let prereqs = stack.pop();  //[1,2]
+            let prereqs = stack.pop();
             
             for (let i = 0; i < prereqs.length; i++) {
                 let prereq = prereqs[i];
