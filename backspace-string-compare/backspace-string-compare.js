bed56cf4d7ed73e07bfc3d3f2e483ed60e8054f2
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-09-06 23:49:23 -0400

    Time: 141 ms (5.27%), Space: 40.5 MB (20.73%) - LeetHub

---
+++ backspace-string-compare/backspace-string-compare.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {    
+    const backSpace = (string) => {
+        let result = string; //ab## -> a + #
+        
+        while (result.indexOf('#') >= 0)  {
+            if (result.indexOf('#') === 0) {
+                result = result.substring(1, result.length);
+            } else {
+                result = result.substring(0, result.indexOf('#') - 1) + result.substring(result.indexOf('#') + 1, string.length);
+            }
+          
+            
+        }
+        
+        return result;
+    }
+    
+    return backSpace(t) === backSpace(s) ? true : false;
+};
