cb668c3d6ca2ac73fe15287d96d67ae9d63841c2
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-09-12 21:45:06 -0400

    Time: 106 ms (27.12%), Space: 39.2 MB (99.31%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.js
+++ maximum-product-subarray/maximum-product-subarray.js
@@ -3,38 +3,34 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    //Naive solution: sliding window from start to end, calculating each product and storing it. Once pointer reaches end, move start pointer up. Repeat until start = end. Then take the max of stored products. 
+    //[2, 3, -2, 0, -4, -5, -7]
     
-    //O(n) Solution
-    //Loop forwards building product, checking max every time
-    //Loop backwards building product, checking max every time
-    //If a zero is met, reset product to 1
-    //Problem dependent on even or odd number of negative integers
-    //[-3, -1, -10] -> loop forwards, max -3*-1 = 3, loop backwards, max -10*-1 = 10
-    //[-2,-3,-2,4] -> -2, 6, -12, -48, 4, -8, 24, -48 = 24
+    if (nums.length === 1) return nums[0];
     
+    let maxProduct = nums[0];
+    let prod = 1;
     
-        let prod = 1;
-        let result = Math.max(...nums);
+    for (let i = 0; i < nums.length; i++) {
+        prod *= nums[i];
         
-        for (let i = 0; i < nums.length; i++) {
-            prod = prod * nums[i];
-            result = Math.max(prod, result);
-            if (prod === 0) {
-                prod = 1;
-            }
+        maxProduct = Math.max(maxProduct, prod);
+        
+        if (nums[i] === 0) {
+            prod = 1;
         }
+    }
     
-        prod = 1;
+    prod = 1;
+    
+    for (let j = nums.length - 1; j >= 0; j--) {
+        prod *= nums[j];
         
-        for (let j = nums.length - 1; j >= 0; j--) {
-            prod = prod * nums[j];
-            result = Math.max(prod, result);
-            if (prod === 0) {
-                prod = 1;
-            }      
+        maxProduct = Math.max(maxProduct, prod);
+        
+        if (nums[j] === 0) {
+            prod = 1;
         }
-        
-        return result;
+    }
     
+    return maxProduct;
 };
