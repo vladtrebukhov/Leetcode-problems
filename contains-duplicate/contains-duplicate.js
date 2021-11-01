7f53520d44898fa6c3ca17c91bddb76d162233a6
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-01 19:48:48 -0400

    Time: 1644 ms (9.25%), Space: 41 MB (93.28%) - LeetHub

--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -3,13 +3,8 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a,b) => a - b);
-    
-    for (let i = 0; i < nums.length - 1;i++) {
-        if (nums[i] === nums[i+1]) {
-            return true;
-        }
-    }
-    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) !== i) return true;
+    } 
     return false;
 };
