951e536131769c83fbdb9209530f38d96aff2c8c
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-01 20:12:54 -0400

    Time: 88 ms (85.78%), Space: 40.7 MB (75.23%) - LeetHub

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -4,15 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
+    if (s.length !== t.length) return false; 
     
-    s = s.split('').sort();
-    t = t.split('').sort();
+    let sMap = {}; 
     
     for (let i = 0; i < s.length; i++) {
-        if (s[i] !== t[i]) return false;
+        sMap[s[i]] = sMap[s[i]] + 1 || 1;
+    }
+    
+    let tMap = {}; 
+    
+    for (let j = 0; j < t.length; j++) {
+        tMap[t[j]] = tMap[t[j]] + 1 || 1;
+    }
+    
+    for (let char in sMap) {
+        if (sMap.hasOwnProperty(char)) {
+            if (!tMap[char] || tMap[char] !== sMap[char]) {
+                return false;
+            }
+        }
     }
     
     return true;
-    
 };
