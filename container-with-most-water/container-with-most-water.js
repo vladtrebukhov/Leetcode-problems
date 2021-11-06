19280490bdbffcf9f9c52750809f0ace571929bc
Vlad Trebukhov <vlad.trebukhov123@gmail.com>
2021-11-06 12:03:51 -0400

    Time: 101 ms (45.47%), Space: 47.9 MB (59.49%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -6,20 +6,20 @@
 var maxArea = function(height) {
     //To find max area, we must maximize length * height
     //Given different heights and their distance from each other, we want the distance from left and right to be as large as possible, while making the height as large as possible. 
-    //At points left and right, the height must be the lower number
+    //At points left and right, the height must be the lower number to encapsulate the water on both sides.
     
     //Start with two pointers, left = 0, right = height.length - 1
-    //while left <= right, keep moving pointer depending on which is taller calculating maxArea each iteration 
+    //while left <= right, keep moving pointer depending on which is shorter 
+    //if height at left shorter, move left pointer, if right, move right -> calculating maxArea each iteration 
     
     let left = 0;
     let right = height.length - 1;
     let maxArea = 0;
     
-    //[1,8,6,2,5,4,8,3,7]
-    
     while (left <= right) {
-        let length = right - left; //7
-        let hgt = Math.min(height[left], height[right]); //8, 7 -> 7
+        let length = right - left;
+        let hgt = Math.min(height[left], height[right]);
+        
         maxArea = Math.max(hgt * length, maxArea);
         height[left] < height[right] ? left++ : right--;
     }
