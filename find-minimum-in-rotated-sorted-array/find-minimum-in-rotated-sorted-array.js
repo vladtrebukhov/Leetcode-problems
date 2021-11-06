957a45de22cd73dcfc153261a5684c27c570c24d
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-06 13:22:45 -0400

    Time: 68 ms (92.62%), Space: 38.8 MB (61.76%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,45 +3,44 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    //[0, 1]
-    //[1, 0]
-    //[7,0,1]
-    //[3,5,6,0,1,2]
-    //[0,1,2,4,5,6,7]
-    //[4,5,6,0]
-    //[1,2,3,4,5]
-    //[0, 0, 0]
-    //[3, 4, 5, 1, 2]
+    //Sorted so we must use binary search -> O(logn)
+    
+    //Value at start will always be larger than end if pivoted
+    
+    //3,4,5 
+    //5,1,2 
+    //5,1,2,3,4 
+    //3,4,5,6,0
     //[4,5,6,7,0,1,2]
-    //[5,1,2,3,4]
+    //[11,13,15,17]
+    //[2,1]
     
-    //if mid is less than mid - 1, we found the pivot thus the smallest value is at mid
-    //if start < end, decrement end 
-    //if start > end, increment start
-    //
     
-    if (nums.length === 1 || nums[0] <= nums[nums.length - 1]) return nums[0]
+    //if start > end and mid > end, on the right
+    //if start > end and mid < end, on the left
+    //if start < end and mid < end, on the left
     
     let start = 0;
     let end = nums.length - 1;
     
-    while (start <= end) {
+    if (nums[start] < nums[end]) return nums[0];
+    
+    while (start < end) {
         let mid = Math.floor((start + end) / 2);
         
-        if (nums[mid] < nums[mid - 1]) {
-            return nums[mid];
+        if (nums[start] > nums[end] && nums[mid] > nums[end]) {
+            start = mid + 1;
         }
         
-        if (nums[mid] > nums[mid + 1]) {
-            return nums[mid + 1];
-        }
-        
-        if (nums[start] > nums[end]) {
-            start++;
+        if (nums[start] > nums[end] && nums[mid] < nums[end]) {
+            end = mid;
         }
         
         if (nums[start] < nums[end]) {
-            end--;
-        } 
+            end = mid - 1;
+        }
     }
+    
+    return nums[start];
+
 };
