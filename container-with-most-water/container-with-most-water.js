b231aec467501b4bb40b097966339766bea6263b
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-06 11:56:50 -0400

    Time: 101 ms (45.47%), Space: 47.9 MB (59.49%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -2,36 +2,27 @@
  * @param {number[]} height
  * @return {number}
  */
-//height = [1,8,6,2,5,4,8,3,7]
-//49
+
 var maxArea = function(height) {
-    if (!height) return 0;
+    //To find max area, we must maximize length * height
+    //Given different heights and their distance from each other, we want the distance from left and right to be as large as possible, while making the height as large as possible. 
+    //At points left and right, the height must be the lower number
     
-    //if first pointer smaller than second pointer, multiply distance between second and first times value at first pointer
-    //then move first pointer up 1
+    //Start with two pointers, left = 0, right = height.length - 1
+    //while left <= right, keep moving pointer depending on which is taller calculating maxArea each iteration 
     
-    //if second pointer is smaller than first pointer, multiply distance between second and first times value at second pointer
-    //then move second pointer down 1
-    
-    //each time check max area against current max
-    
+    let left = 0;
+    let right = height.length - 1;
     let maxArea = 0;
-    let start = 0;
-    let end = height.length - 1;
     
-    while (start < end) {
-        let area;
-        
-        if (height[start] <= height[end]) {
-            area = (height[start] * (end - start));
-            start++;
-        } else {
-            area = (height[end] * (end - start));
-            end--;
-        }
-        maxArea = Math.max(maxArea, area);
+    //[1,8,6,2,5,4,8,3,7]
+    
+    while (left <= right) {
+        let length = right - left; //7
+        let hgt = Math.min(height[left], height[right]); //8, 7 -> 7
+        maxArea = Math.max(hgt * length, maxArea);
+        height[left] < height[right] ? left++ : right--;
     }
     
-    return maxArea; 
-    
+    return maxArea;
 };
