c29c6fd47b41648d39618a36a37c2d352bc5d9a0
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-14 20:27:45 -0500

    Time: 68 ms (100.00%), Space: 42.1 MB (61.82%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.js
+++ top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,25 +4,45 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) { 
-    const freq_map = {};
-    const result = [];
+    //Trivial answer with sorting -> O(nlogn) because sorting, O(1) space
+    //Create map, sort map by highest value
+    //Loop through map and return value where i + 1 = k
+    
+    //Heap -> O(klogn)
+    //Create a frequency hash map from nums
+    //Build a max heap of size k using key,values from map
+    //While count < k, pop off k elements and return values in array
+    
+    //Bucket sort algorithm -> O(n) time, O(n) space
+    //Create a frequency map from nums, create an array of arrays of length of map keys. 
+    //Loop through map, use each value from map as index in array. Push each key from map at corresponding index
+    //Loop through array from the back and return k number of values
+
+    let map = {}
     
     for (let i = 0; i < nums.length; i++) {
-        freq_map[nums[i]] = freq_map[nums[i]] + 1 || 1;
-    };
+        map[nums[i]] = map[nums[i]] + 1 || 1;
+    }
     
-    let order_arr = [...Array(Math.max(...Object.values(freq_map)) + 1)].map(value => value = []);
+    //{1:3, 2: 2, 3: 1, 4: 5}
     
-
-    for (let key in freq_map) {
-        if (freq_map.hasOwnProperty(key)) {
-            order_arr[freq_map[key]].push(key);
+    //->[3, 2, 1, 5]
+    //->[null, null, null, null, null, null] -> need the + 1 because theres never gonna be a freq of 0
+    //->[[],[],[],[],[],[]]
+    
+    let bucket = [...Array(Math.max(...Object.values(map)) + 1)].map(value => value = []);
+    
+    for (let key in map) {
+        if (map.hasOwnProperty(key)) {
+            bucket[map[key]].push(key);
         }
     }
     
-    for (let j = order_arr.length - 1; j >= 0; j--) {
-        for (let l = 0; l < order_arr[j].length; l++) {
-            result.push(order_arr[j][l]);
+    let result = [];
+    
+    for (let j = bucket.length - 1; j >= 0; j--) {
+        for (let l = 0; l < bucket[j].length; l++) {
+            result.push(bucket[j][l])
             if (result.length === k) {
                 return result;
             }
