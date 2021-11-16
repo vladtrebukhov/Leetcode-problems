65687b9ee7fd90bf8351d30f2935536a8b06ce1c
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-15 21:33:58 -0500

    Time: 68 ms (89.98%), Space: 40.2 MB (7.40%) - LeetHub

--- same-tree/same-tree.js
+++ same-tree/same-tree.js
@@ -12,12 +12,8 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-   if (!p && !q) {
-        return true;
-    }
-    if (!p || !q || p.val !== q.val) {
-        return false;
-    }
+    if (!p && !q) return true;
+    if (p && !q || !p && q || p.val !== q.val) return false;
+    
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-   
 };
