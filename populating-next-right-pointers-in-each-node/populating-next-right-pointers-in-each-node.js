c9255c55c7111f5d7360cb0b768a4910650a3c5e
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-11-28 21:32:41 -0500

    Time: 100 ms (63.56%), Space: 45.7 MB (33.59%) - LeetHub

---
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,42 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+
+var connect = function(root) {
+    if (!root) return null;
+    
+    let queue = [root];
+    
+    while (queue.length) {
+        let len = queue.length;
+        
+        for (let i = 0; i < len; i++) {
+            let node = queue.shift();
+            
+            i === len - 1 ?  node.next = null : node.next = queue[0];
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+            
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+    }
+    
+    
+    return root;
+    
+};
