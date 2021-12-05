1be28b2f879f77d0918bc81b4c56efbb6703e926
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-12-05 00:11:01 -0500

    Time: 84 ms (31.92%), Space: 40.4 MB (6.64%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -5,35 +5,44 @@
  */
 var search = function(nums, target) {
     
-    let start = 0
+    let start = 0;
     let end = nums.length - 1;
-    let first = nums[0];
-    let last = nums[nums.length - 1];
     
-    //[1,2,3,4,5]
-    //[4,5,6,7,0,1,2]
-    
+    //[0,1,2,4,5,6,7]
+    //[8,1,2,3,4]
     while (start < end) {
+        let mid = Math.floor((start + end) / 2);
         
-        let mid = Math.floor((start + end) / 2)
-        
-        if (nums[mid] === target) return mid;
-        
-        if (nums[start] <= nums[mid]) {
-            if (target >= nums[start] && target < nums[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        if (nums[mid] >= nums[end]) {
+            start = mid + 1;
         } else {
-            if (target > nums[mid] && target <= nums[end]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            end = mid;
         }
-      
     }
     
-    return nums[start] === target ? start : -1
+    console.log(start, end)
+    if (nums[0] < nums[nums.length - 1]) {
+        end = nums.length - 1;
+    } else {
+        if (target >= nums[0] && target >= nums[end]) {
+            start = 0;
+        } else {
+            end  = nums.length - 1;
+        }    
+    }
+    
+   
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (target < nums[mid]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    
+    return - 1;
 };
