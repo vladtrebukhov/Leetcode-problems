874b79d91cd95ede134b1020175ffea3ca90a5d1
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-12-19 01:09:10 -0500

    Time: 128 ms (96.86%), Space: 51.3 MB (63.78%) - LeetHub

--- sort-an-array/sort-an-array.js
+++ sort-an-array/sort-an-array.js
@@ -6,9 +6,7 @@ var sortArray = function(nums) {
     //If curr > next, swap, else keep iterating
     //Else while curr < prev, keep swapping curr with prev
     //Save start index as we'll be decrementing i. Once finished, reset i to initial i
-    //O(n^2) -> could swap ever element then last element could be smallest, so has to be moved 
-    //all the way to the start from the end
-    
+    //O(n^2) -> Insertion Sort
 //     let startIndex;
     
 //     for (let i = 0; i < nums.length - 1; i++) {
@@ -30,5 +28,55 @@ var sortArray = function(nums) {
     
 //     return nums;
     
+    //O(nlogn) -> Quick sort
+    //An element is sorted if all elements to the left are <= and all elements to the right are >= 
+    //Pick first element as Pivot. left and right pointers move until left > pivot and right < pivot
+    //Swap the elements
+    //Continue until right is past left. Swap right with pivot
+    //Run recursively from start to pivot and from pivot to end
+                           
+    //[5, 8, 7, 12, 12, 14, 15]
+    
+    //[1,2,3,5]
+   
+//     const partition = (left, right) => {
+//         let pivot = nums[left];
+        
+//         let i = left;
+//         let j = right;
+        
+//         while (i < j) {
+//             while (nums[i] <= pivot) {
+//                 i++;
+//             }
+            
+//             while(nums[j] >= pivot) {
+//                 j--;
+//             }
+            
+//             if (i < j) {
+//                 [nums[i], nums[j]] = [nums[j], nums[i]];
+//             }
+//         }
+        
+//         [nums[left], nums[j]] = [nums[j], nums[left]];
+        
+//         return j;
+//     }
+    
+
+         
+//     const quickSort = (left, right) => {
+//         if (left < right) {
+//             let mid = partition(left, right);
+//             quickSort(left, mid);
+//             quickSort(mid+1, right);
+//         }
+//         return nums;
+//     }
+    
+//     return quickSort(0, nums.length - 1);
+    
     return nums.sort((a,b) => a - b)
+    
 };
