71bbe9ceb6f7d797e623c22b4a7d27b1258bdd70
Vlad Trebukhov <vlad.trebukhov123@gmail.com>
2021-12-21 20:07:30 -0500

    Time: 128 ms (42.6%), Space: 55.6 MB (25.06%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.js
+++ find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,7 +3,18 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
+//     Must contain numbers between 1 and length of array (inclusive)
+//     If duplicate exists, one number between 1 and length must be missing
+//     If duplicate exists, nums[first_index] !== nums[current_index]
+        
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums.indexOf(nums[i]) !== i) return nums[i];
+//     }
+    
+    let map = {};
+    
     for (let i = 0; i < nums.length; i++) {
-        if (nums.indexOf(nums[i]) !== i) return nums[i];
+        if (nums[i] in map) return nums[i];
+        map[nums[i]] = i;
     }
 };
