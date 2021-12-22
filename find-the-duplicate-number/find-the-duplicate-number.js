92aea5e49ec6d1f4e823d0a5bd4ec607627e863c
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2021-12-21 19:48:09 -0500

    Time: 9452 ms (5.08%), Space: 48.1 MB (57.75%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.js
+++ find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,9 +3,7 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    nums.sort((a,b) => a - b);
-    
     for (let i = 0; i < nums.length; i++) {
-        if (nums.indexOf(nums[i]) !== i) return nums[i]
+        if (nums.indexOf(nums[i]) !== i) return nums[i];
     }
 };
