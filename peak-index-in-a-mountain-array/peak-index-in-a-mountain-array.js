61669175531c169739a650d31b99b9e43a8c36c2
Vlad Trebukhov <vlad.trebukhov123@gmail.com>
2021-12-28 21:25:43 -0500

    Time: 76ms (56.33%), Space: 40.3 MB (76.76%) - LeetHub

--- peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
@@ -3,7 +3,29 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    for (let i = 1; i < arr.length-1; i++) {
-        if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) return i;
+//     Trivial O(n)
+//     for (let i = 1; i < arr.length-1; i++) {
+//         if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) return i;
+//     }
+    
+//     Binary search (log(n)) -> read problem statement carefully to understand valid input for Bin Search to work
+    
+    let start = 0;
+    let end = arr.length - 1;
+    
+    if (arr[2] < arr[1]) return 1;
+    
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2); 
+        
+        if (arr[mid+1] !== undefined &&
+            arr[mid-1] !== undefined &&
+            arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) {
+            return mid;
+        } else if (arr[mid] >= arr[mid-1]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
 };
