c57fac2129acd0016bf92fb36ce1a0617490aefe
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2022-01-10 20:41:21 -0500

    Time: 190 ms (5.10%), Space: 46.6 MB (5.10%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const output = [];
+    const parens = "()";
+    
+    if (n === 1) return [parens];
+    
+    const isValid = (parens) => {
+        let stack = [];
+        
+        for (let i = 0; i < parens.length; i++) {
+           if (parens[i] === '(') {
+                stack.push(parens[i]); 
+           } else {
+               if (stack.length === 0) return false;
+               stack.pop();
+           }
+        }
+        
+        return stack.length === 0;
+    };
+    
+    const backtracking = (currParens) => {
+        if (currParens.length === n * 2) {
+            if (isValid(currParens)) {
+                output.push(currParens); 
+            }
+            return;
+        }
+        for (let i = 0; i < parens.length; i++) { 
+            backtracking(currParens + parens[i]);
+        }
+    };
+    
+    backtracking("");
+    
+    console.log(output)
+    return output;
+};
