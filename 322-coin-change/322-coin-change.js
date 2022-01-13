0090c948120eaebfc838003a90d34850bd053f6f
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2022-01-12 20:52:57 -0500

    Time: 157 ms (50.97%), Space: 44.6 MB (46.03%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -7,11 +7,16 @@ var coinChange = function(coins, amount) {
     const fewestCoins = [];
     fewestCoins[0] = 0;
     
+    
+    //[1,2,5] amount = 6
+    
+    //[0,,Infinity,Infinity,Infinity,Infinity,Infinity]
+    
     for (let i = 1; i <= amount; i++) {
         fewestCoins[i] = Infinity;
     };
     
-    for (let i = 0; i <= amount; i++) {
+    for (let i = 1; i <= amount; i++) {
         for (let j = 0; j < coins.length; j++) {
             if (coins[j] <= i) {
                 fewestCoins[i] = Math.min(fewestCoins[i], 1 + fewestCoins[i - coins[j]]);
