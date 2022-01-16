40c02b187c71acf51bc42f0fc9e659194608e321
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2022-01-16 00:24:47 -0500

    Time: 76 ms (90.28%), Space: 41.9 MB (5.56%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.js
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -3,7 +3,13 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    let evens = 0;
+    //Count all occurances of letters
+    //Add all even occurances together since they can go on left and right of center always makeing a longer palindrome
+    //Pick the maximum odd number as center
+    //Loop through all values, whenever an odd is found, add odd - 1 (now even) to counter
+    //Since we need a center if any odds exist, we must add plus 1 to answer because we cant subtract from center initially like the other odd values - 1 (even) that we would add to each side
+    
+    let count = 0;
     let map = {};
     
     for (let i = 0; i < s.length; i++) {
@@ -12,19 +18,15 @@ var longestPalindrome = function(s) {
     }
     
     const values = Object.values(map);
-
-    let odds = Object.values(map).filter(value => value % 2 === 1);
-    
-    let maxOdd = odds.length ? Math.max(...odds) : 0;
-    
-    const initialMaxOdd = maxOdd;
+    const odds = Object.values(map).filter(value => value % 2 === 1);
+    const maxOdd = odds.length ? Math.max(...odds) : 0;
     
     for (let i = 0; i < values.length; i++) {
         let value = values[i];
         
-        if (value % 2 === 0) evens += value;
-        if (value % 2 === 1) maxOdd += (value - 1);
+        if (value % 2 === 0) count += value;
+        if (value % 2 === 1) count += (value - 1);
     }
     
-    return maxOdd > 0 ? maxOdd - (initialMaxOdd - 1) + evens : evens;
+    return maxOdd > 0 ? count + 1 : count;
 };
